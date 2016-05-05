a899ec39225e9039fa7afced072ddfb9d0bba6e1
Santiago26 <santiago26@ya.ru>
2016-05-05 18:26:40 +0300

    v1
    it works

--- count.js
+++ count.js
@@ -1,23 +1,52 @@
-var now = new Date();  //с мин сек
-var startDate = new Date(2016, 3, 28); //полночь
+var startDate = new Date(2016, 4, 4); //полночь
+var now //с мин сек
 var diff = now - startDate;
-
 var web=0,
     app=0,
     sum=0;
+var counter=0;
+var time = 10*60*1000 // 10 min
+//var time = 50*1000 // debug 5 sec
+console.log(time/1000/60+" min");//debug
 
-var dd=now.getDate()-startDate.getDate(),
-    hh=now.getHours()-startDate.getHours(),
-    mm=now.getMinutes()-startDate.getMinutes();
-/*
-раз в десять минут a+=100. раз в 20 минут b+=200
-*/
-if (((mm-mm%10)/10)%2 == 0)
-    ((mm-mm%10)/10)%2
+function calc_init_value(){
+    now = new Date();
+    
+    var dd=now.getDate()-startDate.getDate(),
+        hh=now.getHours()-startDate.getHours(),
+        mm=now.getMinutes()-startDate.getMinutes();
 
-web=100*(mm-mm%10)/10;
-app=200*(mm-mm%10)/10;
-sum=dd*900*24+hh*900+web+app;
-document.getElementById('sum').innerHTML=web+" руб";
-document.getElementById('sum2').innerHTML=app+" руб";
+//    console.log(dd+"д "+hh+"ч "+mm + "м") //debug
+
+    sum = dd*900*24+hh*900+(mm-mm%10)*10;
+    write(sum);
+}
+
+function write(data)
+{
+    document.getElementById('sum').innerHTML=data+" &#8381; ";
+//    console.log(data);
+}
+
+calc_init_value();
+setInterval(function() {
+    
+    if (counter%2==0)
+        {
+        sum+=200;
+            
+//        console.log("+200. "+"Итого:"+sum);
+        }
+    else 
+        {
+        sum+=100;
+//        console.log("+100. "+"Итого:"+sum);
+        }
+    write(sum);
+//    console.log(sum);
+    counter++;
+},time);
+
+
+
 
